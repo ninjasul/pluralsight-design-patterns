221c9eab9d144d72a9445fe19cbbb6fc21a8a1bb
ninjasul@gmail.com <ninjasul@gmail.com>
2019-03-05 10:31:26 +0900

    [plauralsight-design-patterns] singleton: implemented lazy loading

--- src/main/java/creational/_01_singleton/DbSingleton.java
+++ src/main/java/creational/_01_singleton/DbSingleton.java
@@ -2,13 +2,18 @@ package creational._01_singleton;
 
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
 
     private DbSingleton() {
 
     }
 
     public static DbSingleton getInstance() {
+
+        if( instance == null ) {
+            instance = new DbSingleton();
+        }
+
         return instance;
     }
 }
